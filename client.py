59de675407ee0d8f8ed4fb0007aaaf4a88a6bfd4
Apoll011 <tiagorobotik@gmail.com>
2024-06-24 17:04:27 -0100

    Added documentation, Refactored code

--- client.py
+++ client.py
@@ -2,6 +2,34 @@ import json
 import socket
 from promise.promise import Promise
 
+class ApiResponse:
+    response: dict
+    """
+    This is the responce returned by the server
+    """
+    code: int
+    """
+    The code returned by the server:
+        `200`: `Ok`
+        `400`: `Route Not Found`
+        `500`: `Server error`. (Description on responce)
+    """
+    time: float
+    """
+    Time in `seconds` that it took to return an answer
+    """
+
+    def __init__(self, data: dict) -> None:
+        """
+        Initializes the API response.
+
+        Args:
+            data (dict): The response data.
+        """
+        self.response = data["response"]
+        self.code = data["code"]
+        self.time = data["time"]
+
 class ApiClient:
     HOST: str
     PORT: int
@@ -25,10 +53,10 @@ class ApiClient:
         """
         Authenticates the client.
         """
-        promise = self.call_route_async("alive")
+        promise = self.call_route_async("auth")
         promise.then(lambda data: self.__auth(data))
-
-    def __auth(self, data: 'ApiResponse'):
+    
+    def __auth(self, data: ApiResponse):
         """
         Authenticates the client.
 
@@ -37,7 +65,14 @@ class ApiClient:
         """
         if data.response["on"]:
             self.active = True
-
+    
+    def close(self):
+        """
+        Close the server
+        """
+        self.call_route("close")
+        self.active = False
+  
     def call_route_async(self, route, value: str | dict[str, str] = ""):
         """
         Calls a route asynchronously.
@@ -49,15 +84,12 @@ class ApiClient:
         Returns:
             A Promise object.
         """
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.HOST, self.PORT))
-        d = {"route": route, "value": value}
-        s.send(bytes(json.dumps(d).encode("utf-8")))
+        s = self.__conect_and_send(route, value)
         promise = Promise()
         promise.resolve(lambda: self.__get_info(s, promise))
         return promise
 
-    def call_route(self, route, value: str | dict[str, str] = ""):
+    def call_route(self, route: str, value: str | dict[str, str] = ""):
         """
         Calls a route synchronously.
 
@@ -68,13 +100,17 @@ class ApiClient:
         Returns:
             An ApiResponse object.
         """
+        s = self.__conect_and_send(route, value)
+        data = s.recv(1024).decode("utf-8")
+        return ApiResponse(json.loads(data))
+
+    def __conect_and_send(self, route: str, value: str | dict[str, str] = ""):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.HOST, self.PORT))
         d = {"route": route, "value": value}
         s.send(bytes(json.dumps(d).encode("utf-8")))
-        data = s.recv(1024).decode("utf-8")
-        return ApiResponse(json.loads(data))
-
+        return s
+    
     def __get_info(self, s, promise: Promise):
         """
         Gets the response from the server.
@@ -95,18 +131,3 @@ class ApiClient:
             s.close()  # Close the socket object
 
 
-class ApiResponse:
-    response: dict
-    code: int
-    time: float
-
-    def __init__(self, data: dict) -> None:
-        """
-        Initializes the API response.
-
-        Args:
-            data (dict): The response data.
-        """
-        self.response = data["response"]
-        self.code = data["code"]
-        self.time = data["time"]
