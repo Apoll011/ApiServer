d9e7328e488492be36b42adace54abd1a4752491
Apoll011 <tiagorobotik@gmail.com>
2024-06-24 15:29:50 -0100

    "Fixed typos, added super() call in __init__, and made minor changes to API class methods."

--- server.py
+++ server.py
@@ -25,10 +25,11 @@ class API(Blueprint):
     PORT: int
     closed: bool
 
-    connected_client_text = "Host conected at #addr#."
-    disconnected_client_text = "Host disconected from #addr#."
+    connected_client_text = "Host connected at #addr#."
+    disconnected_client_text = "Host disconnected from #addr#."
 
     def __init__(self, host: str, port: int):
+        super().__init__()
         self.defs = {}
         self.define_route(host, port)
         self.closed = False
@@ -44,11 +45,11 @@ class API(Blueprint):
         time_s = time.time()
         try:
             if route in self.defs.keys():
-                return json.dumps({"responce": self.defs[route](value),"code": 200, "time": time.time() - time_s})
+                return json.dumps({"response": self.defs[route](value), "code": 200, "time": time.time() - time_s})
             else:
-                return json.dumps({"responce": "invalid", "code": 404, "time":  time.time() - time_s})
+                return json.dumps({"response": "invalid", "code": 404, "time": time.time() - time_s})
         except Exception as e:
-            return json.dumps({"responce": e.__str__(), "code": 500, "time":  time.time() - time_s})
+            return json.dumps({"response": str(e), "code": 500, "time": time.time() - time_s})
 
     def define_route(self, host, port):
         self.HOST = host
@@ -66,11 +67,11 @@ class API(Blueprint):
             self.start_server()
             self.print_header_text(f"Started API Server on address \33[93m{self.HOST}:{self.PORT}", 1)
             self.main_loop()
-            self.server_socket.close()
         except KeyboardInterrupt:
+            self.close()
+        finally:
             self.server_socket.close()
-
-        self.print_header_text(f"Closed API Server", 1)
+            self.print_header_text(f"Closed API Server", 1)
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -81,9 +82,9 @@ class API(Blueprint):
         while not self.closed:
             conn, addr = self.server_socket.accept()
             with conn:
-                self.conect_client(conn, addr)
+                self.connect_client(conn, addr)
 
-    def conect_client(self, conn, addr):
+    def connect_client(self, conn, addr):
         print(f"\33[32m{self.connected_client_text.replace('#addr#', str(addr))}\33[0m")
         self.client_main_loop(conn)
         print(f"\33[31m{self.disconnected_client_text.replace('#addr#', str(addr))}\33[0m")
@@ -92,12 +93,12 @@ class API(Blueprint):
         while True:
             data = conn.recv(1024)
             if not data:
-                continue
+                break
             received_json = json.loads(data.decode("utf-8"))
             response = self.call(received_json["route"], received_json["value"])
             conn.send(response.encode("utf-8"))
-            conn.close()
-            break
+        conn.close()
 
     def close(self):
         self.closed = True
+        self.server_socket.close()
