6246048d12543952a40ee1f773c55f6525ce9353
Apoll011 <tiagorobotik@gmail.com>
2024-07-16 19:33:51 -0100

    New Style Display

--- server.py
+++ server.py
@@ -98,8 +98,9 @@ class API(Blueprint):
     PORT: int
     closed: bool
 
-    connected_client_text = "Host connected at #addr#."
-    disconnected_client_text = "Host disconnected from #addr#."
+    client_name = "Host"
+    conections = 0
+    active = 0
 
     def __init__(self, host: str, port: int):
         """
@@ -155,16 +156,24 @@ class API(Blueprint):
         """
         Starts the API server.
         """
-        os.system("clear")
         try:
             self.__start_server()
-            self.__print_header_text(f"Started API Server on address \33[93m{self.HOST}:{self.PORT}", 1)
+            self.screen()
             self.__main_loop()
         except KeyboardInterrupt:
             self.close()
         finally:
             self.server_socket.close()
             self.__print_header_text(f"Closed API Server", 1)
+    
+    def screen(self):
+        os.system("clear")
+        self.__print_header_text(f"Started API Server on address \33[93m{self.HOST}:{self.PORT}", 1)
+        print(f"Till now there was \33[32m{self.conections}\33[0m from \33[94m{self.client_name}.\33[0m")
+        print(f"There are \33[32m{self.active}\33[0m active conections")
+        print("Active Routes Are: ")
+        for route in self.route_functions:
+            print("\t- \33[33mRoute:\33[32m", route, "\33[36mUp and running\33[0m")
 
     def __start_server(self):
         """
@@ -191,9 +200,12 @@ class API(Blueprint):
             conn (socket.socket): The client connection.
             addr (tuple): The client address.
         """
-        print(f"\33[32m{self.connected_client_text.replace('#addr#', str(addr))}\33[0m")
+        self.conections += 1
+        self.active += 1
+        self.screen()
         self.__client_main_loop(conn)
-        print(f"\33[31m{self.disconnected_client_text.replace('#addr#', str(addr))}\33[0m")
+        self.active -= 1
+        self.screen()
 
     def __client_main_loop(self, conn: socket.socket):
         """
